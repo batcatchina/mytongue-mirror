24ef788587f689363f1e9ec50777f2703b72a417
batcatchina <batcatchina@github.com>
2026-05-14 12:28:30 +0800

    feat: 添加legacy构建-修复微信内置浏览器白屏

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import legacy from '@vitejs/plugin-legacy'
 import path from 'path'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    legacy({
+      targets: ['defaults', 'not IE 11', 'iOS >= 12', 'Android >= 5'],
+      additionalLegacyPolyfills: ['regenerator-runtime/runtime'],
+    }),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
